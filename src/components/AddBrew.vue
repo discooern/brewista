dfc11c09423bb411af329e71e5ddc8899a250ba2
discooern <jona834n@edu.campusvejle.dk>
2021-06-21 14:37:17 +0200

    added some components

---
+++ src/components/AddBrew.vue
@@ -0,0 +1,41 @@
+<template>
+    <span class="flex w-12/12">
+      <a href="#">
+        <svg class="mx-5 my-10" id="arrowBack" style="width: 40px;" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
+        </svg>
+      </a>
+      <h1 class="self-center mx-14"><strong>Pick your Coffee</strong></h1>
+    </span>
+    <a href="#">
+        <img src="../assets/header-coffeecollective-dark@2x.png">
+        <div class="rounded-b-3xl pb-5 mb-8" style="background-color: #293D3B;">
+        <span class="flex flex-col items-start mx-8">
+        <h2 class="text-blue-600">KIENI ESPRESSO</h2>
+        <p class="text-white p-2 text-left">En frugtbombe af en espresso med aroma af brombær, solbær, stikkelbær og fyldig. olieret afslutning.</p>
+        </span>
+        <ul class="flex flex-wrap">
+            <li class="p-8 flex flex-col items-start">"><h3 class="text-white">PROCESS</h3><p class="text-blue-600">Washed</p></li>
+            <li class="p-8 flex flex-col items-start">"><h3 class="text-white">ROAST PROFILE</h3><p class="text-blue-600">Medium</p></li>
+            <li class="p-8 flex flex-col items-start">"><h3 class="text-white">VARIETIES</h3><p class="text-blue-600">Castilo, Caturra</p></li>
+            <li class="p-8 flex flex-col items-start">"><h3 class="text-white">COUNTRY</h3><p class="text-blue-600">Guatemala</p></li>
+        </ul>
+        </div>
+    </a>
+
+    <a href="#">
+        <img src="../assets/header-coffeecollective@2x.png">
+        <div class="rounded-b-3xl pb-5 mb-8" style="background-color: #C5C4BF;">
+        <span class="flex flex-col items-start mx-8">
+        <h2 class="text-gray-800">KIENI ESPRESSO</h2>
+        <p class="text-gray-500 p-2 text-left">En frugtbombe af en espresso med aroma af brombær, solbær, stikkelbær og fyldig. olieret afslutning.</p>
+        </span>
+        <ul class="flex flex-wrap">
+            <li class="p-8 flex flex-col items-start"><h3 class="text-gray-500">PROCESS</h3><p class="text-gray-800">Washed</p></li>
+            <li class="p-8 flex flex-col items-start"><h3 class="text-gray-500">ROAST PROFILE</h3><p class="text-gray-800">Medium</p></li>
+            <li class="p-8 flex flex-col items-start"><h3 class="text-gray-500">VARIETIES</h3><p class="text-gray-800">Castilo, Caturra</p></li>
+            <li class="p-8 flex flex-col items-start"><h3 class="text-gray-500">COUNTRY</h3><p class="text-gray-800">Guatemala</p></li>
+        </ul>
+        </div>
+    </a>
+</template>
